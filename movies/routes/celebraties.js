5e42126e0e955738fabdbda8484863ef5340c7be
michellemeinert <michelle_meinert@yahoo.de>
2019-04-30 10:49:43 +0200

    still not working

--- movies/routes/celebraties.js
+++ movies/routes/celebraties.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Celebraty = require('.././models/celebraties')
 
 
+
 // GET '/celebraties'
 router.get('/', (req, res, next)=> {
   // console.log('i am here', Celebraty)
@@ -15,36 +16,35 @@ router.get('/new', (req, res, next)=>{
   res.render('./celebraties/new')
 })
 
+
+router.post('/new', (req, res, next) => {
+  console.log('inside')
+  const {name, occupation, catchphrase} = req.body;
+  Celebraty.create({name, occupation, catchphrase})
+  .then( (result) => {
+    console.log(result);
+    res.redirect('/celebraties');
+  })
+  .catch(err => console.log(err));
+});
+
 router.get('/:id', (req, res, next)=>{
   const {id} = req.params;
   
   Celebraty.findById(id)
- // .populate('celebraty')
+  // .populate('celebraty')
   .then( (celebraty) => res.render('./celebraties/show', {celebraty}))
   .catch( (err)=> console.log(err))
 })
 
 router.post('/:id/delete', (req, res, next) => {
   const {id} = req.params;
+  
   Celebraty.findByIdAndRemove({id})
-  .then(() => {
-    res.redirect('/');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-});
+  .then( () =>  res.redirect('/'))
+  .catch((err) =>   console.log(err))
+})
 
-router.post('/celebraties', (req, res, next) => {
-  const { name, occupation, catchphrase } = req.body;
-  const celebrity = { name, occupation, catchphrase };
-  Celebrity.create(celebrity)
-  .then(result => {
-    console.log(result);
-    res.redirect('/');
-  })
-  .catch(err => console.log(err));
-});
 
 
 
